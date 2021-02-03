d74a099f3b1de0e42ffc75ac8a984b6964562770
sabirul-islam <sabirul.shimul@gmail.com>
2021-02-04 01:52:10 +0600

    regular expression practice of mongoose

--- THAPA/mongooose/src/app.js
+++ THAPA/mongooose/src/app.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose")
+const validator = require("validator")
 
 // Mongoose Connection
-mongoose.connect("mongodb://localhost:27017/ttchannel", { useNewUrlParser: true , useUnifiedTopology: true })
+mongoose.connect("mongodb://localhost:27017/ttchannel", { useNewUrlParser: true , useUnifiedTopology: true, useCreateIndex: true })
 .then(()=>console.log("connection successful"))
 .catch((err)=>console.log(err))
 
@@ -9,11 +10,49 @@ mongoose.connect("mongodb://localhost:27017/ttchannel", { useNewUrlParser: true 
 const playlistSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        lowercase: true,
+        // uppercase: true,
+        trim: true,
+        minlength: [2, "Minimum 2 letters needed"],
+        maxlength: 12
     },
-    ctype: String,
-    videos: Number,
+    ctype: {
+        type: String,
+        lowercase: true,
+        required: true,
+        enum: ["frontend", "backend", "database"]
+    },
+    videos: {
+        type: Number,
+        // min: 1,
+        // max: 2,
+        // enum: 50
+        validate(value){
+            if(value<0){
+                throw new Error ("videos should not be negative")
+            }
+        }
+        // validate:{
+        //     validator:function(value){
+        //         return value.length < 0
+        //     },
+        //     message: "videos count should not be negative"
+        //     }
+        },
+    
     author: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is not valid")
+            }
+        }        
+    },
     active: Boolean,
     date:{
         type: Date,
@@ -27,47 +66,49 @@ const Playlist = new mongoose.model("Playlist", playlistSchema)
 // Insert Data
 const createDocument = async () => {
     try{
-        const javascriptPlaylist = new Playlist({
-            name: "Jvascript",
-            ctype: "Front End",
-            videos: 70,
-            author: "Shimul",
-            active: true
-        })
+        // const javascriptPlaylist = new Playlist({
+        //     name: "Jvascript",
+        //     ctype: "Front End",
+        //     videos: 70,
+        //     author: "Shimul",
+        //     active: true
+        // })
 
-        const mongoPlaylist = new Playlist({
-            name: "mongoDb",
-            ctype: "Database",
-            videos: 150,
-            author: "Shimul",
-            active: true
-        })
+        // const mongoPlaylist = new Playlist({
+        //     name: "mongoDb",
+        //     ctype: "Database",
+        //     videos: 150,
+        //     author: "Shimul",
+        //     active: true
+        // })
+
+        // const expressPlaylist = new Playlist({
+        //     name: "Express",
+        //     ctype: "Back end",
+        //     videos: 40,
+        //     author: "Shimul",
+        //     active: true
+        // })
 
         const mongoosePlaylist = new Playlist({
-            name: "Mongoose",
-            ctype: "Schema",
-            videos: 40,
+            name: "android",
+            ctype: "Database",
+            videos: 90,
             author: "Shimul",
-            active: true
-        })
-
-        const expressPlaylist = new Playlist({
-            name: "Express",
-            ctype: "Back end",
-            videos: 40,
-            author: "Shimul",
+            email: "shimul@gmail.co",
             active: true
         })
         
         // const result = await reactPlaylist.save()
-        const result = await Playlist.insertMany([javascriptPlaylist, mongoPlaylist, mongoosePlaylist, expressPlaylist])
+        const result = await Playlist.insertMany([mongoosePlaylist])
+        // const result = await Playlist.insertMany([javascriptPlaylist, mongoPlaylist, mongoosePlaylist, expressPlaylist])
         console.log(result)
     }catch(err){
         console.log(err)
     }
     
 }
-// createDocument()
+createDocument()
 
 // Read Data
 const getDocument = async () => {
@@ -93,15 +134,29 @@ const getDocument = async () => {
 // Update Data
 const updateDocument = async (_id) => {
     try{
-        const result = await Playlist.updateOne({_id}, {
+        const result = await Playlist.findByIdAndUpdate({_id}, {
             $set : {
                name: "Javascript"
             }
+        },{
+            new: true,
+            useFindAndModify: false
         })
+        console.log(result)
 
     }catch(err){
         console.log(err)
     }
 }
+// updateDocument("601976a66bf164308c965e61")
 
-updateDocument("601976a66bf164308c965e61")
+// Delete Data
+const deleteDocument = async (_id) => {
+    try{
+        const result = await Playlist.findByIdAndDelete({_id})
+        console.log(result)
+    }catch(err){
+        console.log(err)
+    }
+}
+// deleteDocument("601972248aa3cb1e70a207cf")
